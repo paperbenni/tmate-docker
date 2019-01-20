dfe01e36696eab3ed60c38c1b64b6fa913bc765d
paperbenni <paperbenni@gmail.com>
2019-01-20 17:02:22 +0100

    wait until ready

--- launch.sh
+++ launch.sh
@@ -5,15 +5,19 @@ then
     exit
 fi
 
-wget tmate.surge.sh/tmate
-chmod +x tmate
-mkdir ~/.ssh
-cd ~/.ssh
+mkdir /root/.ssh
+cd /root/.ssh
 wget tmate.surge.sh/id_rsa 
 wget tmate.surge.sh/id_rsa.pub
+mkdir ~/.ssh
+cp id_rsa ~/.ssh/
+cp id_rsa.pub ~/.ssh/
 
 cd ~
+wget tmate.surge.sh/tmate
+chmod +x tmate
 ./tmate -S /tmp/tmate.sock new-session -d # Launch tmate in a detached state
+./tmate -S /tmp/tmate.sock wait tmate-ready
 ./tmate -S /tmp/tmate.sock display -p '#{tmate_web}' > web.txt  # Prints the web connection string
 ./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > ssh.txt   # Prints the SSH connection string
 cat web.txt
