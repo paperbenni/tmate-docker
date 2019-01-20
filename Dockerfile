ea62a9362fa91deeb6eb668b3beb24f6fdea95cc
paperbenni <paperbenni@gmail.com>
2019-01-20 16:51:12 +0100

    works now

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM ubuntu
-RUN apt update && apt install -y openssh wget && mkdir ~/.ssh
+RUN mkdir -p /home/user
+WORKDIR /home/user
+ENV HOME /home/user
+RUN apt-get update && apt-get install -y locales openssh-server openssh-client wget && mkdir ~/.ssh
+RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+    locale-gen
+ENV LANG en_US.UTF-8  
+ENV LANGUAGE en_US:en  
+ENV LC_ALL en_US.UTF-8     
+
 COPY launch.sh launch.sh
-RUN chmod +x launch
+RUN chmod +x launch.sh
 CMD ./launch.sh && bash
--- launch.sh
+++ launch.sh
@@ -4,13 +4,18 @@ then
     echo "wget must be installed"
     exit
 fi
+
 wget tmate.surge.sh/tmate
 chmod +x tmate
 mkdir ~/.ssh
 cd ~/.ssh
-wget tmate tmate.surge.sh/id_rsa 
-wget tmate tmate.surge.sh/id_rsa.pub
+wget tmate.surge.sh/id_rsa 
+wget tmate.surge.sh/id_rsa.pub
 
+cd ~
 ./tmate -S /tmp/tmate.sock new-session -d # Launch tmate in a detached state
-./tmate -S /tmp/tmate.sock display -p '#{tmate_web}' | tee web.txt  # Prints the web connection string
-./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' | tee ssh.txt   # Prints the SSH connection string
+./tmate -S /tmp/tmate.sock display -p '#{tmate_web}' > web.txt  # Prints the web connection string
+./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > ssh.txt   # Prints the SSH connection string
+cat web.txt
+echo " "
+cat ssh.txt
