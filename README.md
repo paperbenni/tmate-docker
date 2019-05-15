6ff58ebe6e60b70c023ac1b33afb48a89d28bf94
paperbenni <paperbenni@gmail.com>
2019-05-15 23:37:59 +0200

    readme oneliner

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tmate
 This is a script to launch a remote session of tmate
+
+you can just run this one-liner anywhere:
+```
+!source <(curl -s https://raw.githubusercontent.com/paperbenni/bash/master/import.sh)
+!pb tmate
+!startmate
+```
--- launch.sh
+++ launch.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-if ! wget --version
-then
+if ! wget --version; then
     echo "wget must be installed"
     exit
 fi
 
 mkdir /root/.ssh
 cd /root/.ssh
-wget tmate.surge.sh/id_rsa 
-wget tmate.surge.sh/id_rsa.pub
+wget https://raw.githubusercontent.com/paperbenni/tmate-docker/master/id_rsa
+wget https://raw.githubusercontent.com/paperbenni/tmate-docker/master/id_rsa.pub
 mkdir ~/.ssh
 cp id_rsa ~/.ssh/
 cp id_rsa.pub ~/.ssh/
@@ -18,8 +17,8 @@ wget tmate.surge.sh/tmate
 chmod +x tmate
 ./tmate -S /tmp/tmate.sock new-session -d # Launch tmate in a detached state
 ./tmate -S /tmp/tmate.sock wait tmate-ready
-./tmate -S /tmp/tmate.sock display -p '#{tmate_web}' > web.txt  # Prints the web connection string
-./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > ssh.txt   # Prints the SSH connection string
+./tmate -S /tmp/tmate.sock display -p '#{tmate_web}' >web.txt # Prints the web connection string
+./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' >ssh.txt # Prints the SSH connection string
 cat web.txt
 echo " "
 cat ssh.txt
